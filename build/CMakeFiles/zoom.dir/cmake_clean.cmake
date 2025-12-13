bcfdc6c9555b7a5d434686fc69d543f481b786ce
ameyamandhare <ameyamandhare17@gmail.com>
2025-12-13 13:12:07 -0500

    Add project files

---
+++ build/CMakeFiles/zoom.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zoom.dir/main.cpp.o"
+  "CMakeFiles/zoom.dir/main.cpp.o.d"
+  "zoom"
+  "zoom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zoom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
